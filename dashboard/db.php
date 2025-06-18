5940122cb6345a23cee00f5c2a5fcc9500c40cf3
CatzLoverz <atmaja123putra@gmail.com>
2025-06-18 12:23:55 +0700

    Remove charset setting from database connection in db.php and dbpublic.php

--- dashboard/db.php
+++ dashboard/db.php
@@ -5,7 +5,6 @@ $password = "admin123";
 $dbname = "senat_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-$conn->set_charset("utf8mb4_uca1400_ai_ci");
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
